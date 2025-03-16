b56ae7072b219102a54be8208abbf453ee2833e4
Nguyen Phan Hoang Quan <hq16101971@gmail.com>
2025-03-16 21:52:57 +0700

    fix AddPost

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -3,9 +3,13 @@ import Image from "next/image"
 const AddPost = () => {  
     return (  
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">AddPost
-      <Image src="https://genk.mediacdn.vn/k:2015/1-78918-47c60bff6263d078621b9e023c6616fe-1450690114301/lien-minh-huyen-thoai-faker-dang-kho-mau-tai-rank-han-quyet-doi-lai-vi-tri-top-1.jpg" alt="" className="w-12 h-12 object-cover rounded-full">
-
-      </Image>
+      <Image 
+        src="https://genk.mediacdn.vn/k:2015/1-78918-47c60bff6263d078621b9e023c6616fe-1450690114301/lien-minh-huyen-thoai-faker-dang-kho-mau-tai-rank-han-quyet-doi-lai-vi-tri-top-1.jpg" 
+        alt="Profile picture" 
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
     </div>
     )  
 }  
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -2,8 +2,16 @@ import Image from 'next/image';
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://genk.mediacdn.vn/k:2015/1-78918-47c60bff6263d078621b9e023c6616fe-1450690114301/lien-minh-huyen-thoai-faker-dang-kho-mau-tai-rank-han-quyet-doi-lai-vi-tri-top-1.jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Blyat</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://genk.mediacdn.vn/k:2015/1-78918-47c60bff6263d078621b9e023c6616fe-1450690114301/lien-minh-huyen-thoai-faker-dang-kho-mau-tai-rank-han-quyet-doi-lai-vi-tri-top-1.jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Blyat</span>
+        </div>
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image src="https://genk.mediacdn.vn/k:2015/1-78918-47c60bff6263d078621b9e023c6616fe-1450690114301/lien-minh-huyen-thoai-faker-dang-kho-mau-tai-rank-han-quyet-doi-lai-vi-tri-top-1.jpg" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
           <span className="font-medium">Blyat</span>
